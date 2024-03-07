f18ae4330e8f200cf3d546b4a793bdaea3cd6095
50 <jonofeasby4@gmail.com>
2024-03-07 15:01:07 +0000

    snx testing

--- TxExecution/Synthetix/SynthetixPositionController.py
+++ TxExecution/Synthetix/SynthetixPositionController.py
@@ -10,6 +10,10 @@ class SynthetixPositionController:
     def __init__(self):
         self.client = get_synthetix_client()
 
+        #######################
+        ### WRITE FUNCTIONS ###
+        #######################
+
     def execute_trade(self, opportunity, is_long: bool, trade_size: float):
         full_asset_name = get_full_asset_name(opportunity['symbol'])
         trade_size_in_asset = get_asset_amount_for_given_dollar_amount(full_asset_name, trade_size)
@@ -23,13 +27,26 @@ class SynthetixPositionController:
         inverse_size = size * -1
         self.client.perps.commit_order(inverse_size, symbol, submit=True)
 
+    def add_collateral(self):
+        self.client.spot.wrap(10000, market_name='sUSDC')
 
-    def get_available_collateral(self) -> float:
+    def create_account(self):
+        self.client.perps.create_account(submit=True)
+
+    def collateral_approval(self):
+        perps_address = self.client.spot.market_proxy.address
+        approve_tx = self.client.spot.approve(perps_address, market_name='sUSDC', submit=True)
+        print(approve_tx)
+
+    ######################
+    ### READ FUNCTIONS ###
+    ######################
+
+    def get_available_collateral(self):
         account = self.get_default_account()
         balances = self.client.perps.get_collateral_balances(account)
-        susd_balance = balances["sUSD"]
 
-        return susd_balance
+        return balances
 
     def get_default_account(self):
         default_account = self.check_for_accounts()
@@ -44,9 +61,6 @@ class SynthetixPositionController:
         else:
             return account_ids 
 
-    def create_account(self):
-        self.client.perps.create_account(submit=True)
-
     def is_already_position_open(self) -> bool:
         positions = self.client.perps.get_open_positions()
         if not positions: 
@@ -58,5 +72,5 @@ class SynthetixPositionController:
 
 
 test = SynthetixPositionController()
-ids = test.check_for_accounts()
-print(ids)
+test.add_collateral()
+
