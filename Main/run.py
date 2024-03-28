75de1fa422f4db250e379317230b1db804d8c34b
50 <jonofeasby4@gmail.com>
2024-03-28 10:29:04 +0000

    setup

---
+++ Main/run.py
@@ -0,0 +1,8 @@
+from Main.main_class import Main
+
+def run():
+    x = Main()
+    x.search_for_opportunities()
+
+if __name__ == "__main__":
+    run()
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     install_requires=requirements,
     entry_points={
         'console_scripts': [
-            'project-run = SynthetixFundingRateArbitrage.Main.main_class',
+            'project-run = SynthetixFundingRateArbitrage.Main.run:run',
         ],
     },
 
