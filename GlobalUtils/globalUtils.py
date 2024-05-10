7a803e53ebf5394b5ce58be5069acbe8cbe298e7
50 <jonofeasby4@gmail.com>
2024-05-10 15:00:24 +0100

    cleanup

--- GlobalUtils/globalUtils.py
+++ GlobalUtils/globalUtils.py
@@ -1,6 +1,3 @@
-import sys
-sys.path.append('/Users/jfeasby/SynthetixFundingRateArbitrage')
-
 from web3 import *
 import os
 from dotenv import load_dotenv
--- TxExecution/Master/MasterPositionControllerUtils.py
+++ TxExecution/Master/MasterPositionControllerUtils.py
@@ -1,4 +1,3 @@
-import sys
 import os
 from dotenv import load_dotenv
 from GlobalUtils.logger import logger
