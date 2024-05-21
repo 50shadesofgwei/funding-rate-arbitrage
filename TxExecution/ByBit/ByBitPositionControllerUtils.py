c9fcbc3200315acdb4f1a9c2c347fa21eab9b3c8
50 <jonofeasby4@gmail.com>
2024-05-21 15:01:59 +0100

    Bybit tx execution

---
+++ TxExecution/ByBit/ByBitPositionControllerUtils.py
@@ -0,0 +1,49 @@
+import os
+from GlobalUtils.logger import logger
+from dotenv import load_dotenv
+load_dotenv()
+
+def get_side(is_long: bool) -> str:
+        if is_long:
+            side = 'Buy'
+            return side
+        else:
+            side = 'Sell'
+            return side
+
+def get_opposite_side(side: str) -> str:
+    try:
+        if side == 'Buy' or side == 'Sell':
+            if side == 'Buy':
+                opposite_side = 'Sell'
+            elif side == 'Sell':
+                opposite_side = 'Buy'
+            return opposite_side
+        else:
+            logger.error(f"ByBitPositionControllerUtils - get_opposite_side called with an argument that is neither 'Buy' nor 'Sell', arg = {side}.")
+            return None
+    except Exception as e:
+            logger.error(f"ByBitPositionControllerUtils - Failed to get opposite side for input: {side}. Error: {e}")
+            return None
+
+
+def is_leverage_already_correct(leverage_factor: float) -> bool:
+    system_leverage = float(os.getenv('TRADE_LEVERAGE'))
+    if system_leverage == leverage_factor:
+        return True
+    else:
+        return False
+
+def parse_close_order_data_from_position_response(APIresponse: dict) -> dict:
+    symbol = APIresponse['result']['list'][0]['symbol']
+    side = APIresponse['result']['list'][0]['side']
+    size = APIresponse['result']['list'][0]['size']
+    opposite_side = get_opposite_side(side)
+    
+    close_order = {
+        'symbol': symbol,
+        'side': opposite_side,
+        'size': size
+    }
+
+    return close_order
