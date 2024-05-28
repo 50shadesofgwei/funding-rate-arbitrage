85b2416a4d18893d487af8a257bfe2866d1b37b7
50 <jonofeasby4@gmail.com>
2024-05-28 01:42:00 +0100

    position open handling

--- TxExecution/HMX/HMXPositionControllerUtils.py
+++ TxExecution/HMX/HMXPositionControllerUtils.py
@@ -1,5 +1,6 @@
 from hmx2.constants.markets import *
 from GlobalUtils.logger import logger
+from GlobalUtils.globalUtils import get_price_from_pyth
 
 def get_market_for_symbol(symbol: str):
     asset_mapping = {
@@ -17,6 +18,25 @@ def get_market_for_symbol(symbol: str):
     market = asset_mapping.get(symbol)
     if market is None:
         logger.error(f"HMXPositionControllerUtils - No market found for symbol: {symbol}")
+        return None
+    return market
+
+def get_symbol_for_market(market: int):
+    asset_mapping = {
+        ARBITRUM_MARKET_BTC_USD: 'BTC',
+        ARBITRUM_MARKET_ETH_USD: 'ETH',
+        ARBITRUM_MARKET_SOL_USD: 'SOL',
+        ARBITRUM_MARKET_W_USD: 'W',
+        ARBITRUM_MARKET_ENA_USD: 'ENA',
+        ARBITRUM_MARKET_DOGE_USD: 'DOGE',
+        ARBITRUM_MARKET_1000PEPE_USD: 'PEPE',
+        ARBITRUM_MARKET_ARB_USD: 'ARB',
+        ARBITRUM_MARKET_BNB_USD: 'BNB',
+    }
+
+    market = asset_mapping.get(market)
+    if market is None:
+        logger.error(f"HMXPositionControllerUtils - No symbol found for market: {market}")
         return None
     return market
 
