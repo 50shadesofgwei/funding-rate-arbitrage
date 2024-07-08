08ff8da1eef08d08ecc749f246cbfeb40a140d91
50 <jonofeasby4@gmail.com>
2024-07-08 14:42:04 +0100

    GMX position controller funcs

--- TxExecution/GMX/GMXPositionController.py
+++ TxExecution/GMX/GMXPositionController.py
@@ -4,9 +4,7 @@ from gmx_python_sdk.scripts.v2.order.order_argument_parser import (
     OrderArgumentParser
 )
 from gmx_python_sdk.scripts.v2.order.create_increase_order import IncreaseOrder
-from gmx_python_sdk.scripts.v2.gmx_utils import ConfigManager
-from decimal import Decimal
-from gmx_python_sdk.scripts.v2.get.get_markets import Markets
+from gmx_python_sdk.scripts.v2.order.create_decrease_order import DecreaseOrder
 from gmx_python_sdk.scripts.v2.get.get_open_positions import GetOpenPositions
 from gmx_python_sdk.scripts.v2.gmx_utils import *
 set_paths()
@@ -14,7 +12,7 @@ set_paths()
 class GMXPositionController:
     def __init__(self):
         self.config = ARBITRUM_CONFIG_OBJECT
-        self.config.set_config()
+        self.config.set_config('/Users/jfeasby/SynthetixFundingRateArbitrage/config.yaml')
 
     def execute_trade(self, opportunity: dict, is_long: bool, trade_size: float):
         try:
@@ -31,7 +29,7 @@ class GMXPositionController:
                     parameters
             )
 
-            order = IncreaseOrder(
+            IncreaseOrder(
                 config=self.config,
                 market_key=order_parameters['market_key'],
                 collateral_address=order_parameters['start_token_address'],
@@ -50,9 +48,60 @@ class GMXPositionController:
             logger.error(f'GMXPositionController - Failed to execute trade for symbol {symbol}. Error: {e}')
             return None
 
+    def close_position(self, symbol: str, reason: str = None):
+        out_token = "USDC"
+        slippage_percent = 0.003
+        amount_of_position_to_close = 1
+        amount_of_collateral_to_remove = 1
+
+        positions = self.get_open_positions(self.config)
+        position = filter_positions_by_symbol(positions, symbol)
+        is_long = position['is_long']
+
+        order_parameters = transform_open_position_to_order_parameters(
+            self.config,
+            positions,
+            symbol,
+            is_long,
+            slippage_percent,
+            out_token,
+            amount_of_position_to_close,
+            amount_of_collateral_to_remove)
+
+
+        DecreaseOrder(
+            config=self.config,
+            market_key=order_parameters['market_key'],
+            collateral_address=order_parameters['collateral_address'],
+            index_token_address=order_parameters['index_token_address'],
+            is_long=order_parameters['is_long'],
+            size_delta=order_parameters['size_delta'],
+            initial_collateral_delta_amount=(
+                order_parameters['initial_collateral_delta']
+            ),
+            slippage_percent=order_parameters['slippage_percent'],
+            swap_path=order_parameters['swap_path'],
+            debug_mode=True
+        )
+
     ######################
     ### READ FUNCTIONS ###
     ######################
+
+    def get_open_positions(self) -> dict:
+        try:
+            address = '0xa6D1FEda6fc70680816eF6A23faf5e454e2f9C09'
+            positions = GetOpenPositions(config=self.config, address=address).get_data()
+
+            if len(positions) > 0:
+                return positions
+            else:
+                return None
+
+        except Exception as e:
+            logger.error(f"GMXPositionController - Failed to get open positions: {e}")
+            return None
+
 
     def is_already_position_open(self) -> bool:
         try:
@@ -72,3 +121,4 @@ class GMXPositionController:
         except Exception as e:
             logger.error(f"GMXPositionController - Error while checking if position is open: {e}", exc_info=True)
             return None
+
--- TxExecution/GMX/GMXPositionControllerUtils.py
+++ TxExecution/GMX/GMXPositionControllerUtils.py
@@ -1,5 +1,7 @@
 from GlobalUtils.logger import *
-
+from gmx_python_sdk.scripts.v2.gmx_utils import *
+from gmx_python_sdk.scripts.v2.get.get_markets import Markets
+from decimal import Decimal
 
 def get_params_object_from_opportunity_dict(opportunity: dict, is_long: bool, trade_size: float) -> dict:
     try:
@@ -18,4 +20,93 @@ def get_params_object_from_opportunity_dict(opportunity: dict, is_long: bool, tr
     
     except Exception as e:
         logger.error(f'GMXPositionControllerUtils - Failed to build a parameters object from opportunity. Error: {e}')
+        return None
+
+def transform_open_position_to_order_parameters(
+    config,
+    positions: dict,
+    market_symbol: str,
+    is_long: bool,
+    slippage_percent: float,
+    out_token,
+    amount_of_position_to_close,
+    amount_of_collateral_to_remove
+):
+
+    direction = "short"
+    if is_long:
+        direction = "long"
+
+    position_dictionary_key = "{}_{}".format(
+        market_symbol.upper(),
+        direction
+    )
+
+    try:
+        raw_position_data = positions[position_dictionary_key]
+        gmx_tokens = get_tokens_address_dict(config.chain)
+
+        collateral_address = find_dictionary_by_key_value(
+            gmx_tokens,
+            "symbol",
+            raw_position_data['collateral_token']
+        )["address"]
+
+        gmx_tokens = get_tokens_address_dict(config.chain)
+
+        index_address = find_dictionary_by_key_value(
+            gmx_tokens,
+            "symbol",
+            raw_position_data['market_symbol'][0]
+        )
+        out_token_address = find_dictionary_by_key_value(
+            gmx_tokens,
+            "symbol",
+            out_token
+        )['address']
+        markets = Markets(config=config).get_available_markets()
+
+        swap_path = []
+
+        if collateral_address != out_token_address:
+            swap_path = determine_swap_route(
+                markets,
+                collateral_address,
+                out_token_address
+            )[0]
+        size_delta = int(int(
+            (Decimal(raw_position_data['position_size']) * (Decimal(10)**30))
+        ) * amount_of_position_to_close)
+
+        return {
+            "chain": config.chain,
+            "market_key": raw_position_data['market'],
+            "collateral_address": collateral_address,
+            "index_token_address": index_address["address"],
+            "is_long": raw_position_data['is_long'],
+            "size_delta": size_delta,
+            "initial_collateral_delta": int(int(
+                raw_position_data['inital_collateral_amount']
+            ) * amount_of_collateral_to_remove
+            ),
+            "slippage_percent": slippage_percent,
+            "swap_path": swap_path
+        }
+    except KeyError:
+        raise Exception(
+            "Couldn't find a {} {} for given user".format(
+                market_symbol, direction
+            )
+        )
+
+def filter_positions_by_symbol(positions: dict, symbol: str) -> dict:
+    try:
+        filtered_data = {}
+        for key, value in positions.items():
+            if 'market_symbol' in value and symbol in value['market_symbol']:
+                filtered_data[key] = value
+        return filtered_data
+    
+    except Exception as e:
+        logger.error(f'GMXPositionControllerUtils - Failed to filter positions by symbol. Symbol: {symbol}, Error: {e}')
         return None
