a49620e35da41988ecbc2a38529da28c44880749
Long Cheng <crcleativity@gmail.com>
2024-07-18 02:19:54 +0800

    add okx trade and account clients

--- APICaller/Okx/okxUtils.py
+++ APICaller/Okx/okxUtils.py
@@ -3,7 +3,8 @@ import okx
 from dotenv import load_dotenv
 from okx.PublicData import PublicAPI as okxPublicAPI
 from okx.TradingData import TradingDataAPI as okxTradingDataAPI
-
+import okx.Account as Account
+import okx.Trade as Trade
 load_dotenv()
 
 def get_okx_pub_client() -> okxPublicAPI:
@@ -16,5 +17,24 @@ def get_okx_trading_data_client() -> okxTradingDataAPI:
 
 def set_okx_symbol(symbol) -> str:
     return f"{symbol}-USD-SWAP"
+
+def get_okx_account_client() ->Account.AccountAPI:
+    api_key = str(os.getenv('OKX_API_KEY'))
+    secret_key = str(os.getenv('OKX_API_SECRET'))    
+    passphrase = str(os.getenv('OKX_PASSPHRASE'))
+
+    accountAPI = Account.AccountAPI(api_key, secret_key, passphrase, False, 0) # 0 for live trading
+
+    return accountAPI
+
+def get_okx_trade_client() ->Trade.TradeAPI:
+    api_key = str(os.getenv('OKX_API_KEY'))
+    secret_key = str(os.getenv('OKX_API_SECRET'))    
+    passphrase = str(os.getenv('OKX_PASSPHRASE'))
+
+    tradeAPI = Trade.TradeAPI(api_key, secret_key, passphrase, False, 0) # 0 for live trading
+
+    return tradeAPI
+
 
 
