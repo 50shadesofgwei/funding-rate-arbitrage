48f82bba08eed6deb7e7413e8a0c8017204c71e4
Long Cheng <crcleativity@gmail.com>
2024-07-23 02:41:12 +0800

    set okx data api to real trading mode

--- APICaller/Okx/okxUtils.py
+++ APICaller/Okx/okxUtils.py
@@ -8,11 +8,11 @@ import okx.Trade as Trade
 load_dotenv()
 
 def get_okx_pub_client() -> okxPublicAPI:
-    okx_pub_client = okxPublicAPI()
+    okx_pub_client = okxPublicAPI(flag = '0')
     return okx_pub_client
 
 def get_okx_trading_data_client() -> okxTradingDataAPI:
-    okx_trading_data_client = okxTradingDataAPI()
+    okx_trading_data_client = okxTradingDataAPI(flag = '0')
     return okx_trading_data_client
 
 def set_okx_symbol(symbol) -> str:
