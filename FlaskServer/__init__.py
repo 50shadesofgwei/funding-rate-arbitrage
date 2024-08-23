abd81b07555c0722d63c4f0a8065eb9126dec649
Prabodhcoolkid <prabodh.gyawali2005@gmail.com>
2024-08-23 20:05:11 +0100

    SocketIO integration for logger

--- FlaskServer/__init__.py
+++ FlaskServer/__init__.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from PositionMonitor.TradeDatabase.TradeDatabase import TradeLogger
 from FlaskServer.services import cli_commands, trade_routes, log_routes, settings
 import os
+from flask_socketio import SocketIO, emit
 
 load_dotenv()
 
@@ -11,12 +12,12 @@ load_dotenv()
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     CORS(app)
+    socketio = SocketIO(app)
     app.trade_logger = TradeLogger()
     app.register_blueprint(cli_commands.api_routes)
     app.register_blueprint(trade_routes.routes)
     app.register_blueprint(log_routes.log_blueprint)
     app.register_blueprint(settings.settings_blueprint)
-
 
     if test_config is None:
         # Default configurations
@@ -27,7 +28,7 @@ def create_app(test_config=None):
         # Apply test configurations
         app.config.update(test_config)
     
-    return app
+    return socketio, app
 
 # TODO: Write Route Unit Tests
     # - read advanced flask testing docs
--- FlaskServer/run.py
+++ FlaskServer/run.py
@@ -1,9 +1,35 @@
 from FlaskServer import create_app
+from flask_socketio import SocketIO, emit
 
-app = create_app()
+(sio, app) = create_app()
+
+# SocketIO event handlers
+@sio.on('connect')
+def handle_connect():
+    print('Client connected')
+
+@sio.on('disconnect')
+def handle_disconnect():
+    print('Client disconnected')
+
+@sio.on('log')
+def handle_log(data):
+    print(data)
+    emit('log', data)
+
+# Socket IO events
+@sio.event
+def log(data):
+    """Emits log data to the client.
+    Client should listen for 'log' event.
+    Log data should be popped on LogsTable component in front-end.
+    """
+    emit('log', data)
+    
+
 
 def run():
-    app.run()
+    sio.run(app)
 
 if __name__ == "__main__":
     run()
