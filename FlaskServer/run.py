42e664563049df5699af164438d20dd14f1b2309
Prabodhcoolkid <prabodh.gyawali2005@gmail.com>
2024-08-28 21:18:29 +0100

    project-run fix needed

--- FlaskServer/run.py
+++ FlaskServer/run.py
@@ -1,33 +1,42 @@
 from FlaskServer import create_app
 from flask_socketio import SocketIO, emit
 import logging
-from GlobalUtils.logger import logger, app_formatter
+from GlobalUtils.logger import logger, app_formatter, setup_topics
+from pubsub import pub
+from GlobalUtils.globalUtils import EventsDirectory
 
 (sio, app) = create_app()
 
-# SocketIO event handlers
-@sio.on('connect')
-def handle_connect():
-    print('Client connected')
-    emit('log', 'Connected to server')
+# Setup topics for the application
+setup_topics()
 
-@sio.on('disconnect')
-def handle_disconnect():
-    print('Client disconnected')
+# Create PubSub Listeners for to redirect to SocketIO
+def position_opened_to_socketio(position_data):
+    sio.emit(EventsDirectory.POSITION_OPENED.value, position_data)
 
-@sio.on('log')
-def handle_log(data):
-    print(data)
-    emit('log', data)
+def position_closed_to_socketio(position_report):
+    sio.emit(EventsDirectory.POSITION_CLOSED.value, position_report)
 
-# Socket IO events
-@sio.event
-def log(data):
-    """Emits log data to the client.
-    Client should listen for 'log' event.
-    Log data should be popped on LogsTable component in front-end.
-    """
-    emit('log', data)
+def close_all_positions_to_socketio():
+    sio.emit(EventsDirectory.CLOSE_ALL_POSITIONS.value)
+
+def close_position_pair_to_socketio(symbol, reason, exchanges):
+    sio.emit(EventsDirectory.CLOSE_POSITION_PAIR.value, {"symbol": symbol, "reason": reason, "exchanges": exchanges})
+
+def opportunity_found_to_socketio(opportunity):
+    sio.emit(EventsDirectory.OPPORTUNITY_FOUND.value, opportunity)
+
+def trade_logged_to_socketio(position_data):
+    sio.emit(EventsDirectory.TRADE_LOGGED.value, position_data)
+
+# Subscribe to all topics and forward to SocketIO
+pub.subscribe(position_opened_to_socketio, EventsDirectory.POSITION_OPENED.value)
+pub.subscribe(position_closed_to_socketio, EventsDirectory.POSITION_CLOSED.value)
+pub.subscribe(close_all_positions_to_socketio, EventsDirectory.CLOSE_ALL_POSITIONS.value)
+pub.subscribe(close_position_pair_to_socketio, EventsDirectory.CLOSE_POSITION_PAIR.value)
+pub.subscribe(opportunity_found_to_socketio, EventsDirectory.OPPORTUNITY_FOUND.value)
+pub.subscribe(trade_logged_to_socketio, EventsDirectory.TRADE_LOGGED.value)
+
 
 # Custom handler for logging to the SocketIO
 class SocketIOHandler(logging.Handler):
